829384ab4210184ad1525833e1e0760df010df50
Facundo <facundopohle@gmail.com>
2025-04-30 13:53:07 -0300

    aoskdfdsa

--- soundfront/src/components/footer.jsx
+++ soundfront/src/components/footer.jsx
@@ -5,6 +5,8 @@ import logo from "../img/logo.png"
 import logo1 from "../img/logo1.png"
 import logo2 from "../img/logo2.png"
 import logo3 from "../img/eraser.png"
+import logo4 from "../img/iluminati.png"
+import logo5 from "../img/paris.png"
 
 const Footer = () => {
 
@@ -23,6 +25,11 @@ const Footer = () => {
 
                     <ul className="col-4 list-unstyled footer_icons_container">
                         <li className="ms-3">
+                            <a href="https://www.instagram.com/eraser.prod?igsh=MWNxcW01MnhzY2h5cA==" target="_blank" rel="noopener noreferrer">
+                                <img src={logo3} className="logo1" alt="Instagram Erase" />
+                            </a>
+                        </li>
+                        <li className="ms-3">
                             <a href="https://www.instagram.com/aqv_productora?igsh=bzR3a3hyYmZuM2c3" target="_blank" rel="noopener noreferrer">
                                 <img src={logo1} className="logo1" alt="Instagram AQV Productora" />
                             </a>
@@ -32,14 +39,19 @@ const Footer = () => {
                                 <img src={logo2} className="logo1" alt="Instagram Poison777 Recs" />
                             </a>
                         </li>
+                        {/* <li className="ms-3 mt-4">
+                            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                            <i className="fa-brands fa-facebook fa-xl text-white footer_icon"></i>
+                            </a>
+                            </li> */}
                         <li className="ms-3">
-                            <a href="https://www.instagram.com/eraser.prod?igsh=MWNxcW01MnhzY2h5cA==" target="_blank" rel="noopener noreferrer">
-                                <img src={logo3} className="logo1" alt="Instagram Erase" />
+                            <a href="https://www.instagram.com/poison777recs?igsh=OXd1dWRmOGl6Zzcy" target="_blank" rel="noopener noreferrer">
+                                <img src={logo4} className="logo2" alt="Instagram Poison777 Recs" />
                             </a>
                         </li>
-                        <li className="ms-3 mt-4">
-                            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-                                <i className="fa-brands fa-facebook fa-xl text-white footer_icon"></i>
+                        <li className="ms-3">
+                            <a href="https://www.instagram.com/poison777recs?igsh=OXd1dWRmOGl6Zzcy" target="_blank" rel="noopener noreferrer">
+                                <img src={logo5} className="logo2" alt="Instagram Poison777 Recs" />
                             </a>
                         </li>
                         <li className="ms-3 mt-4">
@@ -47,11 +59,11 @@ const Footer = () => {
                                 <i className="fa-brands fa-instagram fa-xl text-white footer_icon"></i>
                             </a>
                         </li>
-                        <li className="ms-3 mt-4">
+                        {/* <li className="ms-3 mt-4">
                             <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
                                 <i className="fa-brands fa-linkedin-in fa-xl text-white footer_icon"></i>
                             </a>
-                        </li>
+                        </li> */}
                     </ul>
 
                 </footer>
